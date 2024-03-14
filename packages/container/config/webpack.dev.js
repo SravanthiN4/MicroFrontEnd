1baf6d2978a7e9b2dd288e1a121b5dc291d5af43
SravanthiN4 <snedunoori44@gmail.com>
2024-03-14 16:48:06 -0400

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
@@ -13,9 +12,7 @@ const devConfig = {
         }
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: './public/index.html'
-        }),
+
         new ModuleFederationPlugin({
             name:'container',
             remotes: {
